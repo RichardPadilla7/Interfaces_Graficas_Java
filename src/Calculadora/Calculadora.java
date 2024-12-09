f832dc8c3a5c5d3280b397d6ff8f6740b8829b0d
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-12-09 14:32:59 -0500

    1123

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -1,4 +1,65 @@
 package Calculadora;
 
-public class Calculadora {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculadora extends JPanel{
+    public JPanel Calcu;
+    public JTextArea Resultados;
+    public JButton sumaButton;
+    public JButton restaButton;
+    public JButton multiplicacionButton;
+    public JButton divisionButton;
+    public JTextField numero1;
+    public JTextField numero2;
+
+    public Calculadora() {
+        sumaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(numero1.getText());
+                int num2 = Integer.parseInt(numero2.getText());
+                int suma = num1 + num2;
+                Resultados.setText(String.valueOf(suma));
+
+            }
+        });
+
+        restaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(numero1.getText());
+                int num2 = Integer.parseInt(numero2.getText());
+                int resta = num1 - num2;
+                Resultados.setText(String.valueOf(resta));
+
+            }
+        });
+
+        multiplicacionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(numero1.getText());
+                int num2 = Integer.parseInt(numero2.getText());
+                int multiplicacion = num1 * num2;
+                Resultados.setText(String.valueOf(multiplicacion));
+
+            }
+        });
+
+        divisionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(numero1.getText());
+                int num2 = Integer.parseInt(numero2.getText());
+                if(num2!= 0){
+                    double division = num1 / num2;
+                    Resultados.setText(String.valueOf(division));
+                }else{
+                    JOptionPane.showMessageDialog(null, "No se puede dividir entre 0", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
 }
--- src/Calculadora/Main.java
+++ src/Calculadora/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package Calculadora;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        JFrame frame = new JFrame("Calculadora");
+        Calculadora calculadora = new Calculadora(); // Instancia de Calculadora
+        frame.setContentPane(calculadora.Calcu); // Establece el panel principal
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300); // Tamaño de la ventana
+        frame.setLocationRelativeTo(null); // Centra la ventana
+        frame.setVisible(true);
     }
 }
